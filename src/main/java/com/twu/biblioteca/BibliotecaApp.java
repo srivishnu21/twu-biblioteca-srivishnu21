febce571be0bb6bd2fbb9db07ea8f3c2b007f160
srivishnu2101-1 <srivishnu.s1@thoughtworks.com>
2020-02-10 15:47:06 +0530

    refactor | added test in bibliotecaAppTest

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class BibliotecaApp {
 
-    static String welcomeMessage="Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-    public static List<Book> bookList = new ArrayList<>(Arrays.asList(new Book("harry potter", 2003, "j.k rowling"),
-            new Book("Two states", 2005, "chetan bghath"),
+    public static List<Book> bookList = new ArrayList<>(Arrays.asList(new Book("harry potter",
+                    2003, "j.k rowling"),
+            new Book("Two states", 2005, "chetan bhagat"),
             new Book("Wings of fire", 2005, "apj")));
+    static String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     static Printer printer = new Printer();
     //TODO: better name for BookKeeper
     static BookKeeper bookKeeper = new BookKeeper(bookList);
     static Menu menu = new Menu(bookKeeper);
 
     public static void main(String[] args) {
-        //TODO: extract as function and test
         printer.print(welcomeMessage);
         menu.execute();
     }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -2,9 +2,9 @@ package com.twu.biblioteca;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class BibliotecaAppTest {
@@ -16,6 +16,27 @@ class BibliotecaAppTest {
 
         BibliotecaApp.main(null);
 
-        verify(mockedPrintStream,times(1)).println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        verify(mockedPrintStream, times(1)).println("Welcome to Biblioteca. " +
+                "Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    @Test
+    void shouldCheckIfMenuExecuteMethodIsCalled() {
+        PrintStream mockedPrintStream = mock(PrintStream.class);
+        System.setOut(mockedPrintStream);
+        ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+
+        BibliotecaApp.main(null);
+
+        verify(mockedPrintStream, times(1)).println("Welcome to Biblioteca. " +
+                "Your one-stop-shop for great book titles in Bangalore!");
+        verify(mockedPrintStream, times(2)).println("Menu options");
+        verify(mockedPrintStream, times(2)).println("1. Display List of all books");
+        verify(mockedPrintStream, times(2)).println("2. Check out a book");
+        verify(mockedPrintStream, times(2)).println("3. Return a book");
+        verify(mockedPrintStream, times(2)).println("Please Enter A Number");
+        verify(mockedPrintStream, times(1)).println("harry potter\t,\t2003\t,\tj.k rowling\n" +
+                "Two states\t,\t2005\t,\tchetan bhagat\nWings of fire\t,\t2005\t,\tapj\n");
     }
 }
