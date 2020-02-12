b0c7dee3f95f5f490d86b9011fe9169c3070de85
srivishnu2101-1 <srivishnu.s1@thoughtworks.com>
2020-02-12 10:49:20 +0530

    added | test that should print rating when it has no rating

--- src/test/java/com/twu/biblioteca/MovieTest.java
+++ src/test/java/com/twu/biblioteca/MovieTest.java
@@ -13,4 +13,12 @@ class MovieTest {
 
         assertEquals(expected, movie.toString());
     }
+
+    @Test
+    void testShouldCheckRatingIsNotAddedWhenItHasNoRating() {
+        Movie movie = new Movie("movie1", 2006, "xyz", -1);
+        String expected = "movie1\t\t2006\t\txyz";
+
+        assertEquals(expected, movie.toString());
+    }
 }
